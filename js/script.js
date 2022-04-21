048335dc294c39a63bf7d20196b6ee19f7907463
Sahil Kesarwani <sahilkesarwani2@gmail.com>
2022-04-22 01:45:58 +0530

    Production

---
+++ js/script.js
@@ -0,0 +1,71 @@
+(function () {
+	var d = document,
+		accordionToggles = d.querySelectorAll(".js-accordionTrigger"),
+		setAccordionAria,
+		switchAccordion,
+		touchSupported = "ontouchstart" in window,
+		pointerSupported = "pointerdown" in window;
+
+	skipClickDelay = function (e) {
+		e.preventDefault();
+		e.target.click();
+	};
+
+	setAriaAttr = function (el, ariaType, newProperty) {
+		el.setAttribute(ariaType, newProperty);
+	};
+	setAccordionAria = function (el1, el2, expanded) {
+		switch (expanded) {
+			case "true":
+				setAriaAttr(el1, "aria-expanded", "true");
+				setAriaAttr(el2, "aria-hidden", "false");
+				break;
+			case "false":
+				setAriaAttr(el1, "aria-expanded", "false");
+				setAriaAttr(el2, "aria-hidden", "true");
+				break;
+			default:
+				break;
+		}
+	};
+
+	switchAccordion = function (e) {
+		e.preventDefault();
+		var thisAnswer = e.target.parentNode.nextElementSibling;
+		var thisQuestion = e.target;
+		if (thisAnswer.classList.contains("is-collapsed")) {
+			setAccordionAria(thisQuestion, thisAnswer, "true");
+		} else {
+			setAccordionAria(thisQuestion, thisAnswer, "false");
+		}
+		thisQuestion.classList.toggle("is-collapsed");
+		thisQuestion.classList.toggle("is-expanded");
+		thisAnswer.classList.toggle("is-collapsed");
+		thisAnswer.classList.toggle("is-expanded");
+
+		thisAnswer.classList.toggle("animateIn");
+	};
+	for (var i = 0, len = accordionToggles.length; i < len; i++) {
+		if (touchSupported) {
+			accordionToggles[i].addEventListener("touchstart", skipClickDelay, false);
+		}
+		if (pointerSupported) {
+			accordionToggles[i].addEventListener("pointerdown", skipClickDelay, false);
+		}
+		accordionToggles[i].addEventListener("click", switchAccordion, false);
+	}
+})();
+
+(function () {
+	const goTop = document.getElementById("go-top");
+
+	window.addEventListener("scroll", function () {
+		var y = window.scrollY;
+		if (y >= this.window.innerHeight) {
+			goTop.style.display = "inline";
+			goTop.classList.add("animate__animated", "animate__fadeIn");
+		} else {
+			goTop.style.display = "none";
+		}
+	});
+})();
